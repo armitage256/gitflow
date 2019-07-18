45f1d2ccc49c3bcd07564c1b229a441099635483
armitage256 <sexeymitagge@gmail.com>
2019-07-18 13:46:43 +0000

    added go, c scripts - sum functions

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+int sum(int x, int y) {
+	return x + y;
+}
+
 void main(void){
-	printf("Hello world!");
+	printf("Hello world!\n");
+	int x = 1, y = 3;
+	printf("sum %d + %d = %d\n", x, y, sum(x, y));
 }
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+func lSum(x, y int) {
+	log.Printf("sum %d + %d = %d\n", x, y, (x + y))
+}
 
 func main(){
 	fmt.Println("Hello World!")
+	lSum(10, 11)
 }
